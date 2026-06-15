bb55de7946040cf46cb4d661c90ab09cb84e688d
MarcosAndradeV <mastermarcos1212@hotmail.com>
2026-06-15 00:20:40 -0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -188,6 +188,31 @@ pub fn draw_text_ex(
 pub fn is_cursor_on_screen() -> bool {
     unsafe { IsCursorOnScreen() }
 }
+pub fn check_collision_circle_rec(center: Vector2, radius: f32, rec: Rectangle) -> bool {
+    unsafe { CheckCollisionCircleRec(center, radius, rec) }
+}
+
+pub fn is_texture_valid(texture: Texture2D) -> bool {
+    unsafe { IsTextureValid(texture) }
+}
+
+pub fn unload_texture(texture: Texture2D) {
+    unsafe {
+        UnloadTexture(texture);
+    }
+}
+
+pub fn draw_texture_ex(
+    texture: Texture2D,
+    position: Vector2,
+    rotation: f32,
+    scale: f32,
+    tint: Color,
+) {
+    unsafe {
+        DrawTextureEx(texture, position, rotation, scale, tint);
+    }
+}
 
 // Some Basic Colors
 // NOTE: Custom raylib color palette for amazing visuals on WHITE background
@@ -566,3 +591,28 @@ pub enum KeyboardKey {
     KEY_VOLUME_DOWN = 25, // Key: Android volume down button
 }
 pub use KeyboardKey::*;
+
+/// System/Window config flags
+/// NOTE: Every bit registers one state (use it with bit masks)
+/// By default all flags are set to 0
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub enum ConfigFlags{
+    FLAG_VSYNC_HINT         = 0x00000040,   // Set to try enabling V-Sync on GPU
+    FLAG_FULLSCREEN_MODE    = 0x00000002,   // Set to run program in fullscreen
+    FLAG_WINDOW_RESIZABLE   = 0x00000004,   // Set to allow resizable window
+    FLAG_WINDOW_UNDECORATED = 0x00000008,   // Set to disable window decoration (frame and buttons)
+    FLAG_WINDOW_HIDDEN      = 0x00000080,   // Set to hide window
+    FLAG_WINDOW_MINIMIZED   = 0x00000200,   // Set to minimize window (iconify)
+    FLAG_WINDOW_MAXIMIZED   = 0x00000400,   // Set to maximize window (expanded to monitor)
+    FLAG_WINDOW_UNFOCUSED   = 0x00000800,   // Set to window non focused
+    FLAG_WINDOW_TOPMOST     = 0x00001000,   // Set to window always on top
+    FLAG_WINDOW_ALWAYS_RUN  = 0x00000100,   // Set to allow windows running while minimized
+    FLAG_WINDOW_TRANSPARENT = 0x00000010,   // Set to allow transparent framebuffer
+    FLAG_WINDOW_HIGHDPI     = 0x00002000,   // Set to support HighDPI
+    FLAG_WINDOW_MOUSE_PASSTHROUGH = 0x00004000, // Set to support mouse passthrough, only supported when FLAG_WINDOW_UNDECORATED
+    FLAG_BORDERLESS_WINDOWED_MODE = 0x00008000, // Set to run program in borderless windowed mode
+    FLAG_MSAA_4X_HINT       = 0x00000020,   // Set to try enabling MSAA 4X
+    FLAG_INTERLACED_HINT    = 0x00010000    // Set to try enabling interlaced video format (for V3D)
+}
+pub use ConfigFlags::*;
